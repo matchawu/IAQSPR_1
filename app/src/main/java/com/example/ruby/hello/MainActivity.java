9e0c3d97e44bd4ade2aaa4dc5ef302de3e072968
matchawu <ruby05075151@hotmail.com>
2018-09-02 01:13:37 +0800

    tabs added

--- app/src/main/java/com/example/ruby/hello/MainActivity.java
+++ app/src/main/java/com/example/ruby/hello/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                textWelcome.setText("HI");
 //                textWelcome.setTextSize(50);
-                Intent intent = new Intent( MainActivity.this, Main3Activity.class);
+                Intent intent = new Intent( MainActivity.this, Tab.class);
                 startActivity(intent);
 
             }
